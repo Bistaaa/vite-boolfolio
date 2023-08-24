45b921304cf56e95582cea679560d98fbb1be00d
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-08-24 15:28:18 +0200

    errore con router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,4 @@
 <template>
   <h1>Projects</h1>
-  <div class="row justify-content-between">
-    <project-card class="my-2" v-for="project in projects" :key="project.id" :project="project" />
-  </div>
+  <router-view />
 </template>
-
-<script>
-import ProjectCard from './components/ProjectCard.vue'
-import axios from 'axios';
-
-const API = "http://localhost:8000/api/v1"
-
-export default {
-  name: 'App',
-  components: {
-    ProjectCard
-  },
-  data() {
-    return {
-      projects: []
-    }
-  },
-  mounted() {
-    axios.get(API + '/projects')
-      .then(res => {
-
-        this.projects = res.data.projects;
-      })
-      .catch(err => console.error(err))
-  }
-}
-</script>
